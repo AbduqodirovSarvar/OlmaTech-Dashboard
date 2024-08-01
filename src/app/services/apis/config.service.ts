aea12fd4df53a050d9d0d9fa8699d3fb01f48eeb
Sarvar Abduqodirov <abduqodirovsarvar.2002@gmail.com>
2024-08-01 16:12:43 +0500

    add api

--- src/app/services/apis/config.service.ts
+++ src/app/services/apis/config.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ConfigService {
-  private baseApiUrl: string = 'http://45.130.148.137:8080/api';
+  private baseApiUrl: string = 'http://192.168.60.45:8080/api';
   constructor() { }
 
   getBaseApiUrl(): string {
