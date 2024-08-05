ea54b7f617f6fbfbe1364e6c68d54ca8056466a3
Sarvar Abduqodirov <abduqodirovsarvar.2002@gmail.com>
2024-08-05 11:19:28 +0500

    change api url

--- src/app/services/apis/config.service.ts
+++ src/app/services/apis/config.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ConfigService {
-  private baseApiUrl: string = 'http://192.168.60.45:8080/api';
+  private baseApiUrl: string = 'https://test.api.olmatech.uz:4443/api';
   constructor() { }
 
   getBaseApiUrl(): string {
